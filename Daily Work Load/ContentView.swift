a7b6e223dc7c91044b125119feda896b9dd426c6
Christopher Huffaker <neo@ctopher.me>
2025-04-10 12:56:43 -0700

    Initial Commit

---
+++ Daily Work Load/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Daily Work Load
+//
+//  Created by Christopher Huffaker on 4/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Daily Work Load/Daily_Work_LoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_Work_LoadApp.swift
+//  Daily Work Load
+//
+//  Created by Christopher Huffaker on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Daily_Work_LoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
